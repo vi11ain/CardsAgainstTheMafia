e293651ed6620e80a375f043fc9452a72ac7548a
Yuval <yuvalgal51@gmail.com>
2020-07-17 18:19:25 +0300

    new design!

--- app.py
+++ app.py
@@ -4,20 +4,38 @@ from yattag import Doc
 with open('answers.txt', encoding='utf-8') as f:
     answers = f.readlines()
 
+with open('questions.txt', encoding='utf-8') as f:
+    questions = f.readlines()
+
 doc, tag, text = Doc().tagtext()
 doc.asis('<!DOCTYPE html>')
-with tag('html', style=""):
+with tag('html'):
     with tag('head'):
         doc.asis('<link rel="stylesheet" href="style.css"/>')
     with tag('body'):
+        # Answer cards back
+        with tag('div', klass='card'):
+            with tag('p', klass='card-back-title'):
+                text('קלפים נגד המאפיה')
+        # Answer cards front
         for answer in answers:
-            with tag('div', klass='answer-card'):
+            with tag('div', klass='card'):
                 with tag('p'):
                     text(answer)
+                doc.asis('<img src="default.png"/>')
+        # Question cards back
+        with tag('div', klass='card question'):
+            with tag('p', klass='card-back-title'):
+                text('קלפים נגד המאפיה')
+        # Question cards front
+        for question in questions:
+            with tag('div', klass='card question'):
+                with tag('p'):
+                    text(question)
                 doc.asis('<img src="default.png"/>')
 
 with open('design/index.html', 'w+', encoding='utf-8') as f:
     f.write(doc.getvalue())
 
-pdfkit.from_file('design\\index.html', 'out.pdf', options={'--margin-bottom': '0mm', '--margin-top': '0mm', '--margin-left': '0mm',
-                                                           '--margin-right': '0mm', '--enable-local-file-access': None, '--page-width': '60mm', '--page-height': '90mm', '--encoding': 'utf-8'})
+pdfkit.from_file('design\\index.html', 'out.pdf', options={'--zoom': 1.261, '--margin-bottom': '0', '--margin-top': '0', '--margin-left': '0',
+                                                           '--margin-right': '0', '--enable-local-file-access': None, '--page-width': '60mm', '--page-height': '90mm', '--encoding': 'utf-8'})
